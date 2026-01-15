a76ad50da4275e922267ee70fbe30cc58ee6cedb
Isenewo Oluwaseyi Ephraim <isenewoephr2012@gmail.com>
2026-01-15 17:01:33 +0100

    feat: modified README.md of project root to include folder structures for each layer of the whole application

--- corporate-platform/corporate-platform-web/src/components/theme/ThemeProvider.tsx
+++ corporate-platform/corporate-platform-web/src/components/theme/ThemeProvider.tsx
@@ -81,27 +81,3 @@ export function ThemeProvider({
     </div>
   )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 'use client'
-
-// import * as React from 'react'
-// import { ThemeProvider as NextThemesProvider } from 'next-themes'
-// import { type ThemeProviderProps } from 'next-themes'
-
-// export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-//   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-// }
